9175e961982caef140fb6f7d5b0678689dccb90a
Vlad <ogay.vlad@gmail.com>
2012-12-02 17:28:49 +0400

    Non-static SolverData: step #12

--- csharp/WakeCode/DataReader.cs
+++ csharp/WakeCode/DataReader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace WakeCode
 {
@@ -10,57 +11,52 @@ namespace WakeCode
         //------------------------------------------------
         public void READ_DATA(SolverData solverData, GeneralData generalData)
         {
-            //GeneralData GeneralData = new GeneralData();
-            //SolverData SolverData = new SolverData();
-            int i, j;
-
-            using (System.IO.FileStream fileStream = System.IO.File.Open("initial_data.inp", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Read))
+            using (var fileStream = File.Open("initial_data.inp", FileMode.OpenOrCreate, FileAccess.Read))
+            using (var streamReader = new StreamReader(fileStream))
             {
-                using (System.IO.StreamReader streamReader = new System.IO.StreamReader(fileStream))
+                READ(streamReader, ref generalData.IMAX); // The number of grid points in x direction
+                READ(streamReader, ref generalData.JMAX); // The number of the grid points in Y direction
+
+                generalData.x = new double[generalData.IMAX];
+                generalData.y = new double[generalData.JMAX];
+                generalData.vell_i = new double[generalData.IMAX,generalData.JMAX];
+
+                READ(streamReader, ref solverData.Dturb); // THE DIAMETER OF THE TURBIN
+                READ(streamReader, ref solverData.H); //  THE HEIGHT OF THE TURBINE
+                READ(streamReader, ref solverData.Ct); // TURBINE THRUST COEFFICIENT
+                READ(streamReader, ref solverData.Kwake); // wake expand scalar
+                READ(streamReader, ref solverData.Uhub);
+                    //m/s - VELOCITY AT THE HUB, WITHOUT THE INFLUENCE OF THE WIND TURBIN
+                READ(streamReader, ref generalData.N_TURB); //THE NUMBER OF THE TURBINE
+
+                generalData.x_turb = new double[generalData.N_TURB];
+                generalData.y_turb = new double[generalData.N_TURB];
+                generalData.R_TURB = new double[generalData.N_TURB];
+                generalData.WPOWER = new double[generalData.N_TURB];
+
+                generalData.xc_turb = new System.Int32[generalData.N_TURB];
+                generalData.yc_turb = new System.Int32[generalData.N_TURB];
+
+                READ(streamReader, ref solverData.Rho); // THE DENSITY OF THE AIR 
+                READ(streamReader, ref solverData.dist); // the distance behind the turbine where the power is computed
+                READ(streamReader, ref generalData.ang);
+                    // rotational angle of the axis: vellocity has the same direction as Ox
+                READ(streamReader);
+                READ(streamReader);
+                for (var i = 0; i <= generalData.N_TURB - 1; i++)
                 {
-                    READ(streamReader, ref generalData.IMAX);               // The number of grid points in x direction
-                    READ(streamReader, ref generalData.JMAX);               // The number of the grid points in Y direction
-
-                    generalData.x = new double[generalData.IMAX];
-                    generalData.y = new double[generalData.JMAX];
-                    generalData.vell_i = new double[generalData.IMAX, generalData.JMAX];
-
-                    READ(streamReader, ref solverData.Dturb);               // THE DIAMETER OF THE TURBIN
-                    READ(streamReader, ref solverData.H);                   //  THE HEIGHT OF THE TURBINE
-                    READ(streamReader, ref solverData.Ct);                  // TURBINE THRUST COEFFICIENT
-                    READ(streamReader, ref solverData.Kwake);               // wake expand scalar
-                    READ(streamReader, ref solverData.Uhub);                //m/s - VELOCITY AT THE HUB, WITHOUT THE INFLUENCE OF THE WIND TURBIN
-                    READ(streamReader, ref generalData.N_TURB);             //THE NUMBER OF THE TURBINE
-
-                    generalData.x_turb = new double[generalData.N_TURB];
-                    generalData.y_turb = new double[generalData.N_TURB];
-                    generalData.R_TURB = new double[generalData.N_TURB];
-                    generalData.WPOWER = new double[generalData.N_TURB];
-
-                    generalData.xc_turb = new System.Int32[generalData.N_TURB];
-                    generalData.yc_turb = new System.Int32[generalData.N_TURB];
-
-                    READ(streamReader, ref solverData.Rho);                         // THE DENSITY OF THE AIR 
-                    READ(streamReader, ref solverData.dist);                        // the distance behind the turbine where the power is computed
-                    READ(streamReader, ref generalData.ang);                        // rotational angle of the axis: vellocity has the same direction as Ox
-                    READ(streamReader);
-                    READ(streamReader);
-                    for (i = 0; i <= generalData.N_TURB - 1; i++)
-                    {
-                        READ(streamReader, ref generalData.x_turb[i], ref generalData.y_turb[i]);   // pozition of the turbine
-                    }
-                    READ(streamReader);
+                    READ(streamReader, ref generalData.x_turb[i], ref generalData.y_turb[i]); // pozition of the turbine
                 }
+                READ(streamReader);
             }
-        }  // END SUBROUTINE READ DATA
+        }
 
-
-        private static void READ(System.IO.TextReader textReader)
+        private static void READ(TextReader textReader)
         {
             textReader.ReadLine();
         }
 
-        private static void READ(System.IO.TextReader textReader, ref int intValue)
+        private static void READ(TextReader textReader, ref int intValue)
         {
             string line = textReader.ReadLine();
             string[] lineParts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -70,7 +66,7 @@ namespace WakeCode
             }
         }
 
-        private static void READ(System.IO.TextReader textReader, ref double doubleValue)
+        private static void READ(TextReader textReader, ref double doubleValue)
         {
             double result;
             string line = textReader.ReadLine();
@@ -82,7 +78,7 @@ namespace WakeCode
             doubleValue = result;
         }
 
-        private static void READ(System.IO.TextReader textReader, ref double doubleValue1, ref double doubleValue2)
+        private static void READ(TextReader textReader, ref double doubleValue1, ref double doubleValue2)
         {
             string line = textReader.ReadLine();
             string[] lineParts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
